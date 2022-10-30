b7904bb698a592dea297860a3070f85d1bf53993
Kilian De Bock <kilian.debock@student.arteveldehs.be>
2022-10-30 15:29:00 +0100

    22.43.7.3: Add more pages to routes and import its content from content generator

--- front-office/src/pages/index.ts
+++ front-office/src/pages/index.ts
@@ -2,3 +2,4 @@ export * from './pages';
 export * from './Home/Home.page';
 export * from './NotFound/NotFound.page';
 export * from './Category/Category.page';
+export * from './ContentPage/Content.page';
