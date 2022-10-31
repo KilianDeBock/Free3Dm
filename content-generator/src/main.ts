61964e32753306751e40e77fc2538dfe3e2f5fc0
Kilian De Bock <kilian.debock@student.arteveldehs.be>
2022-10-31 13:36:14 +0100

    22.44.1.2: Change layout content generator main.ts

--- content-generator/src/main.ts
+++ content-generator/src/main.ts
@@ -1,15 +1,71 @@
 import { generate } from './utils';
 
-generate('pages', '../content/pages', { prefix: '_', suffix: 'Page' });
-generate('buttons', '../content/buttons', { prefix: '_' });
-generate('products', '../content/products', { prefix: '_' });
-generate('reviews', '../content/reviews', { prefix: '_' });
-generate('tutorial', '../content/tutorial', { prefix: '_' });
-generate('errors', '../content/error messages', { prefix: '_' });
-generate('dialogs', '../content/dialog text', { prefix: '_' });
+generate(
+  // Generate pages
+  'pages',
+  '../content/pages',
+  { prefix: '_', suffix: 'Page' },
+);
 
-generate('main/header', '../content/main/header', { prefix: '_' });
-generate('main/footer', '../content/main/footer', { prefix: '_' });
-generate('main/promos', '../content/main/top of page promo banner', {
-  prefix: '_',
-});
+generate(
+  // Generate buttons
+  'buttons',
+  '../content/buttons',
+  { prefix: '_', suffix: 'Button' },
+);
+
+generate(
+  // Generate Products
+  'products',
+  '../content/products',
+  { prefix: '_' },
+);
+
+generate(
+  // Generate reviews
+  'reviews',
+  '../content/reviews',
+  { prefix: '_' },
+);
+
+generate(
+  // Generate tutorial
+  'tutorial',
+  '../content/tutorial',
+  { prefix: '_' },
+);
+
+generate(
+  // Generate errors
+  'errors',
+  '../content/error messages',
+  { prefix: '_' },
+);
+
+generate(
+  // Generate dialogs
+  'dialogs',
+  '../content/dialog text',
+  { prefix: '_' },
+);
+
+generate(
+  // Generate header
+  'main/header',
+  '../content/main/header',
+  { prefix: '_' },
+);
+
+generate(
+  // Generate footer
+  'main/footer',
+  '../content/main/footer',
+  { prefix: '_' },
+);
+
+generate(
+  // Generate promos
+  'main/promos',
+  '../content/main/top of page promo banner',
+  { prefix: '_' },
+);
