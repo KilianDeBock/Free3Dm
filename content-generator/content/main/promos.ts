41d9a8bac3207439a3a0f60c79ff3d51ec4d7f3f
Kilian De Bock <kilian.debock@student.arteveldehs.be>
2022-11-03 16:20:18 +0100

    22.44.4.4: Update content

--- content-generator/content/main/promos.ts
+++ content-generator/content/main/promos.ts
@@ -1,6 +1,6 @@
 export const _FilamentSale = "Start printing now! All filaments are 20% off until December 31st.";
 export const _NewFoodSafeFilaments = "New food-safe PLA filaments! Our food-safe filaments are made with materials and colour pigments that are approved by the FDA and the EFSA.";
-export const _NewTo3DPrinting = "New to 3D printing? Don't know where to start?\nCheck out our <u>Getting Started</u> guide to learn about printers, printing materials, and the best starter products so you can start printing now.";
+export const _NewTo3DPrinting = "New to 3D printing? Don't know where to start?\nCheck out our [Getting Started](\"/getting-started\") guide to learn about printers, printing materials, and the best starter products so you can start printing now.";
 
 export default {
   _FilamentSale, 
