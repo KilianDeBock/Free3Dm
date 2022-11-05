0caea3ad386524c0f525d9d2035106fe830a0747
Kilian De Bock <kilian.debock@student.arteveldehs.be>
2022-11-05 15:36:30 +0100

    22.44.6.4: Update content

--- content-generator/content/errors.ts
+++ content-generator/content/errors.ts
@@ -1,7 +1,7 @@
-export const _EmailAlreadyRegistered = "Your email address is already registered for an account. Would you like to [sign in](\"/login\")?";
+export const _EmailAlreadyRegistered = "Your email address is already registered for an account. Would you like to [sign in](/login)?";
 export const _IncorrectEmailFormat = "Please enter your email using a valid format. *Example:* natalie96@gmail.com";
-export const _IncorrectPassword = "Incorrect password. Please try again or [reset your password](\"/reset-password\").";
-export const _IncorrectUsername = "Incorrect username. Please try again or [register for an account](\"/register\").";
+export const _IncorrectPassword = "Incorrect password. Please try again or [reset your password](/reset-password).";
+export const _IncorrectUsername = "Incorrect username. Please try again or [register for an account](/register).";
 
 export default {
   _EmailAlreadyRegistered, 
